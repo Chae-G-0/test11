3aa5f5b92246dbc844b2a27d584a1db203f9d725
unknown <1510283@donga.ac.kr>
2022-09-27 16:50:36 +0900

    220927

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Footer from './component/Footer';
+import Header from './component/Header';
 import './css/App.css';
 
 function App() {
   return (
     <>
-
+      <Header />
+      
+      <Footer />
     </>
   );
 }
