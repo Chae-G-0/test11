c5f5bc970d0170f0d940d1de7b821ab6e1219625
unknown <1510283@donga.ac.kr>
2022-09-29 00:27:07 +0900

    220929

--- src/pages/Sub01.js
+++ src/pages/Sub01.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../css/Subpage.scss'
+
+const BM = [
+  {id:1, title:"관광사업", link:"#!"},
+  {id:2, title:"마케팅", link:"#!"},
+  {id:3, title:"MICE", link:"#!"},
+  {id:4, title:"관광기업지원센터", link:"#!"},
+]
 
 const Sub01 = () => {
   return (
     <div className='Subpage'>
-      <div className="Sh">
-        
+      <div className="Sh01">
+        <ul>
+          {
+            BM.map(bm => {
+              return(
+                <Link to={bm.link}>
+                  <li key={bm.id}>{bm.title}</li>
+                </Link>
+              )
+            })
+          }
+        </ul>
       </div>
       <h2>주요사업</h2>
     </div>
--- src/pages/Sub02.js
+++ src/pages/Sub02.js
@@ -5,6 +5,9 @@ import '../css/Subpage.scss'
 const Sub02 = () => {
   return (
     <div className='Subpage'>
+      <div className="Sh02">
+        
+      </div>
       <h2>
         부산여행정보
       </h2>
