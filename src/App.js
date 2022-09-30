ab5e024d83e07f96f1dc57c5fc6d10475ae92eb6
chae <corkdud@naver.com>
2022-09-30 17:36:33 +0900

    220930

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Route, Router, Routes } from "react-router-dom";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
@@ -9,13 +10,30 @@ import Sub02 from "./pages/Sub02";
 import Sub03 from "./pages/Sub03";
 
 function App() {
+    const [wet, setWet] = useState();
+
     useEffect(() => {
-        const url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=ywRlJa7ppqDu3r%2BZgaoE4hxgKL03rb%2FZH6YKSCyaOqRJZa%2B7MMiFJBXuSswp2Hph6Go86ji9%2BmET3T%2BKutJnFg%3D%3D&numOfRows=10&pageNo=1&base_date=20220930&base_time=0500&nx=55&ny=127&dataType=JSON"
-    });
+        const url =
+            "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=ywRlJa7ppqDu3r%2BZgaoE4hxgKL03rb%2FZH6YKSCyaOqRJZa%2B7MMiFJBXuSswp2Hph6Go86ji9%2BmET3T%2BKutJnFg%3D%3D&numOfRows=10&pageNo=1&base_date=20220930&base_time=0500&nx=55&ny=127&dataType=JSON";
+        const getWeather = async () => {
+            const res = await axios.get(url).then((res) => setWet(res));
+            const wdata = res.data.map((it) => {
+                return {
+                    date: it.baseDate,
+                    cate: it.category,
+                };
+            });
+
+            // console.log(res.data);
+            // console.log(wdata);
+        };
+        getWeather();
+    }, []);
     return (
         <>
             <Header />
             <Routes>
+                {console.log(wet)}
                 <Route path="/" element={<Main />} />
                 <Route path="/Sub01" element={<Sub01 />} />
                 <Route path="/Sub02" element={<Sub02 />} />
