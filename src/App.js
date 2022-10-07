bd2d1f1e50c81342901127478950b7ff0f52d4a5
chae <corkdud@naver.com>
2022-10-07 17:49:35 +0900

    221007

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
     useEffect(() => {
         const url =
-            `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=ywRlJa7ppqDu3r%2BZgaoE4hxgKL03rb%2FZH6YKSCyaOqRJZa%2B7MMiFJBXuSswp2Hph6Go86ji9%2BmET3T%2BKutJnFg%3D%3D&numOfRows=10&pageNo=1&base_date=${date}&base_time=0500&nx=35&ny=129&dataType=JSON`;
+            `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=ywRlJa7ppqDu3r%2BZgaoE4hxgKL03rb%2FZH6YKSCyaOqRJZa%2B7MMiFJBXuSswp2Hph6Go86ji9%2BmET3T%2BKutJnFg%3D%3D&numOfRows=10&pageNo=1&base_date=${date}&base_time=0500&nx=35&ny=129&dataType=JSON`;
         axios.get(url).then((res) => setWet(res.data.response.body.items.item));
     }, []);
 
