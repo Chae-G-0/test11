a12745c0927ac14f6cfd3ee4739eb20a3db369c3
chae <corkdud@naver.com>
2022-10-18 17:39:20 +0900

    221018

--- src/pages/MainBuss.js
+++ src/pages/MainBuss.js
@@ -3,28 +3,26 @@ import { Link } from "react-router-dom";
 import "../css/MainBuss.scss";
 
 const BUSS = [
-    { id: 1, des: "관광사업", src: "/assets/images/mainCon01.JPG", link: "/Sub01" },
-    { id: 2, des: "MICE사업", src: "/assets/images/mainCon02.JPG", link: "/Sub01" },
-    { id: 3, des: "부산관광기업지원센터", src: "/assets/images/mainCon03.JPG", link: "/Sub01" },
-    { id: 4, des: "마케팅사업", src: "/assets/images/mainCon04.JPG", link: "/Sub01" },
+    { id: 1, des: "세계 최초 2회 연속 '국제 안전 도시' 공인", span: "* 2019년 국제안전도시 공인센터(ISCCC)", src: "/assets/images/mice01.jpg", link: "/Sub01" },
+    { id: 2, des: "국내 최초 스마트시티, 에코델타시티 개발", span: "* 2018년 국토부 선정", src: "/assets/images/mice03.jpg", link: "/Sub01" },
+    { id: 3, des: "부산을 대표하는 MICE 전문시설 벡스코", span: "* 2018 대한민국 안전대상 우수기업부문", src: "/assets/images/mice02.jpg", link: "/Sub01" },
 ];
 
 const MainBuss = () => {
     return (
         <section className="MainBuss csc">
             <div className="inner">
-                <div className="left">
-                    <h2>주요 사업</h2>
-                    <p>부산관광공사는 지역의 역사, 문화, 예술 그리고 도시 전체를 활용한 지속가능관광의 기반을 만들겠습니다.</p>
-                </div>
+                <h2>주요 사업</h2>
+                <p>부산관광공사는 지역의 역사, 문화, 예술 그리고 도시 전체를 활용한 지속가능관광의 기반을 만들겠습니다.</p>
                 <div className="right">
                     {BUSS.map((bs) => {
                         return (
                             <figure key={bs.id}>
-                                <Link to={bs.link}>
-                                    <img src={process.env.PUBLIC_URL + bs.src} alt="" />
-                                </Link>
-                                <div className="des">{bs.des}</div>
+                                <img src={process.env.PUBLIC_URL + bs.src} alt="" />
+                                <div className="des">
+                                    {bs.des}
+                                    <span>{bs.span}</span> 
+                                </div>
                             </figure>
                         );
                     })}
